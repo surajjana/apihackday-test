ed488d26a96fd6ee668dd01ad17843cf6478083a
Suraj Kumar Jana <surajjana2@gmail.com>
2015-12-12 14:25:12 +0530

    sentiments added

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.get('/tweets', function (request, response){
 });
 
 app.get('/sentiment', function (request, response){
-	request("http://www.sitepoint.com", function(error, response, body) {
+	request("https://api.havenondemand.com/1/api/sync/analyzesentiment/v1?text=i+love+my+life&apikey=4c517421-8409-4a33-8b20-cf547c587cf3", function(error, response, body) {
 	  console.log(body);
 	});
 });
